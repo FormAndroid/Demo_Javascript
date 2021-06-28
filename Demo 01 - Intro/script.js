341b1aa7bf18939675b7976a4336d32e0020418b
Aurelien <aurelien.strimelle@bstorm.be>
2021-06-28 04:23:26 -0700

    Demo 01

---
+++ Demo 01 - Intro/script.js
@@ -0,0 +1,29 @@
+// Le « use strict » active une vérification du langage plus stricte!
+'use strict';
+
+/* Ceci est un commentaire
+    sur plusieurs
+    lignes 
+*/
+
+
+
+//### Les interactions avec le navigateur
+
+// - Ecriture dans la console
+console.log('Hello');
+
+console.info('Message d\'info');
+console.warn('Attention, ça va péter :o')
+console.error('BOUM !');
+
+// - Interaction avec l'utilisateur
+// Affichage un popup de message
+alert('Bonjour !');
+
+// Demander la saisie d'une valeur
+const msg = prompt('Veuillez saisir un message !');
+
+// Demande une validation (Oui / Non)
+const valid = confirm('C\'est bon pour vous ?');
+
